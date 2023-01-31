2dbaa1b5b1f4e14d5e80d8dd1a34a36a27c1c9c8
leej1230 <leejaewoo1230@gmail.com>
2023-01-31 00:05:24 -0800

    added title and search bar

---
+++ src/home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import TextField from "@mui/material/TextField";
+import { IconButton } from '@mui/material';
+import SearchIcon from "@mui/icons-material/Search";
+import "./home.css"
+
+
+function home() {
+  return (
+    <div className='home'>
+        <h2>
+            Human Genomics Search
+        </h2>
+        <div className='search'>
+            <TextField
+                id='input_keyword'
+                variant='outlined'
+                fullWidth
+                label="Search by ...?"
+            />
+            <IconButton type="submit" aria-label="search">
+                <SearchIcon style={{ fill: "blue" }} />
+            </IconButton>
+        </div>
+    </div>
+  )
+}
+
+export default home
