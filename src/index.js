5a1ce9de692571b6cc0a28955a7a549e99849c80
leej1230 <leejaewoo1230@gmail.com>
2023-01-31 12:55:00 -0800

    navbar added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Navbar from './navbar'
 import Home from './home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar />
     <Home />
   </React.StrictMode>
 );
---
+++ src/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./navbar.css";
+
+function navbar() {
+  return (
+    <div>
+        <ul>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Contact</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default navbar
