40957d22e606bb819733ce12e64cf140aa216970
potat <73308999+srenduch@users.noreply.github.com>
2023-02-13 11:37:46 -0800

    Basic API GET
    Get sample data from the API endpoint and transmit it to the frontend.
    
    Index samples by sample.id

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import  Home from "./home";
 import  Navbar from "./navbar";
 import Data from "./data";
+import SampleList from './components/SampleList';
 
 function app() {
   return (
@@ -10,6 +11,14 @@ function app() {
         <Routes>
             <Route exact path="/" element={<><Navbar/><Home/></>}/>
             <Route exact path="/data" element={<><Navbar/><Data/></>}/>
+            <Route exact path="/frontend_test" element={
+              <>
+                <Navbar/>
+                <div>
+                  <SampleList/>
+                </div>
+              </>
+            }/>
         </Routes>
     </Router>
   );
