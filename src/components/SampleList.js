85dccc3429fe2220c354570635427ca6a6b0ecd8
potat <73308999+srenduch@users.noreply.github.com>
2023-03-01 08:49:16 -0800

    fix search bug

--- src/components/SampleList.js
+++ src/components/SampleList.js
@@ -12,42 +12,17 @@ function SampleList(props) {
       .then(result => {
         setPatient_data(result.data);
       })
-      // const result = await axios(URL);
-      // result = await result.json()
-      // setPatient_data(result.data);
     }
     fetchPatients()
-    // console.log(patient_data)
   });
-  // state = {
-  //   samples: []
-  // }
-
-  // componentDidMount() {
-  //   axios.get(URL)
-  //     .then(res => {
-  //       const samples = res.data;
-  //       this.setState({ samples });
-  //     })
-  // }
   
   return (
     <div>
       {
         patient_data && patient_data.filter((word) => {
-          return props.kword.toLowerCase() === '' ? word : word.patient_id.toLowerCase().includes(props.kword.toLowerCase())
+          return word.patient_id.toLowerCase().includes(props.kword.toLowerCase())
         }).map((patient) =>
-            <li key={patient.patient_id}><a href={'/data/' + patient.patient_id}>Patient {patient.id} ID: {patient.patient_id}</a>
-              {/* <ul>
-                Gene IDs: {sample.gene_ids}
-              </ul>
-              <ul>
-                Gene Values: {sample.gene_values}
-              </ul>
-              <ul>
-                Dataset ID: {sample.dataset_id}
-              </ul> */}
-            </li>
+            <li><a href={'/data/' + patient.patient_id}>Patient {patient.id} ID: {patient.patient_id}</a></li>
           )
       }
     </div>
