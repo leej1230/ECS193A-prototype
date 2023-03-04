06dc049b58a16eee6fde429ebffb0dda5c14d2b7
mehitaachuthan <machuthan@ucdavis.edu>
2023-03-04 15:48:46 -0800

    added background image

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,32 +7,67 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default class HomePage extends React.Component {
-    
-   render(){
+    render(){
+        return(
+            <div>
+                <ul className='bar'>
+                    <li className='bar'><a href='#/' className='bar'>Home</a></li>
+                    <li className='bar'><a href='#/' className='bar'>About</a></li>
+                    <li className='bar'><a href='#/' className='bar'>Contact</a></li>
+                </ul>
+                <div className="fullPage" style={{ 
+                    backgroundImage: `url("https://wp-cpr.s3.amazonaws.com/uploads/2019/07/618922201_1485610661.jpg")`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    width: '100vw',
+                    height: '100vh',
+                    backgroundOrigin: 'border-box',
+                    backgroundAttachment: 'fixed!important',
+                    overflow: 'hidden'
+                }}>
+            
+                <div >
+                    <h3>{"Genomics Browser"}</h3>
+                </div>
+
+
+                <div >
+                    <h4 >Helping Researchers and Medical Professionals Work With Genomics Data</h4> 
+                </div>
+            </div>
+            </div>
+
+            
+        )
+    }
+   
+}
+
+/*
+
+render(){
        //console.log(this.state.dataset)
      return (
-       <div style={{ 
-        backgroundImage: `url("https://wp-cpr.s3.amazonaws.com/uploads/2019/07/618922201_1485610661.jpg")` 
-      }}>
+        <div className="fullPage" style={{ 
+                backgroundImage: `url("https://wp-cpr.s3.amazonaws.com/uploads/2019/07/618922201_1485610661.jpg")`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                width: '100vw',
+                height: '100vh',
+                backgroundOrigin: 'border-box'
+            }}>
          
-         <div className="titleLayout">
-               <h3>{"Genomics Browser"}</h3>
-         </div>
-
-         <div className="cardLayout">
-           <div className='cardContent'>
-             <h4 className='cardTitle'>Helping Researchers and Medical Professionals Work With Genomics Data</h4> 
-           </div>
-         </div>
+            <div >
+                <h3>{"Genomics Browser"}</h3>
+            </div>
 
 
-        <div className="buttonGroup">
-            <button className="buttonElement"> Login </button>
-            <button className="buttonElement"> Signup </button>
+            <div >
+                <h4 >Helping Researchers and Medical Professionals Work With Genomics Data</h4> 
+            </div>
         </div>
-
-
-       </div>
      )
    }
-}
+   */
