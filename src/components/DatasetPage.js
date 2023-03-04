291533ce2bed93da5d01e5544dc527efb91231c4
mehitaachuthan <machuthan@ucdavis.edu>
2023-03-03 17:48:28 -0800

    icons appear in table

--- src/components/DatasetPage.js
+++ src/components/DatasetPage.js
@@ -11,6 +11,42 @@ import ScrollBars from "react-custom-scrollbars";
 //import { useTable } from "react-table";
 import MaterialTable from 'material-table';
 
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+  Add: AddBox,
+  Check: Check,
+  Clear: Clear,
+  Delete: DeleteOutline,
+  DetailPanel: ChevronRight,
+  Edit: Edit,
+  Export: SaveAlt,
+  Filter: FilterList,
+  FirstPage: FirstPage,
+  LastPage: LastPage,
+  NextPage: ChevronRight,
+  PreviousPage: ChevronLeft,
+  ResetSearch: Clear,
+  Search: Search,
+  SortArrow: ArrowUpward,
+  ThirdStateCheck: Remove,
+  ViewColumn: ViewColumn
+};
+
 export default class DatasetPage extends React.Component {
 
      state = {
@@ -70,7 +106,7 @@ export default class DatasetPage extends React.Component {
             { title: 'So', field: 'surname' },
             { title: 'Do', field: 'birthYear', type: 'numeric' },
             { title: 'DoYeri', field: 'birthCity', lookup: { 34: 'tanbul', 63: 'urfa' } }
-          ]} />
+          ]} icons={tableIcons}/>
           
           <Box className="cardLayout">
             <Card variant="outlined">
