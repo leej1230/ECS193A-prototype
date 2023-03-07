182d2b1ab2b3b3269610d26aca8c66b3e25550e6
mehitaachuthan <machuthan@ucdavis.edu>
2023-03-07 12:34:19 -0800

    modified home page layout

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,10 @@ import { Box, Card , CardContent, CardActions, Typography, Button, Table, TableR
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+import './HomePage.css'
+
 export default class HomePage extends React.Component {
+
     render(){
         return(
             <div>
@@ -16,7 +19,8 @@ export default class HomePage extends React.Component {
                     <li className='bar'><a href='#/' className='bar'>Contact</a></li>
                 </ul>
                 <div className="fullPage" style={{ 
-                    backgroundImage: `url("https://wp-cpr.s3.amazonaws.com/uploads/2019/07/618922201_1485610661.jpg")`,
+                    //backgroundImage: `url("https://wp-cpr.s3.amazonaws.com/uploads/2019/07/618922201_1485610661.jpg")`,
+                    backgroundImage: `url("https://as2.ftcdn.net/v2/jpg/02/44/28/75/1000_F_244287519_RoEredXSEUy46jCwJLAMruvfmweIr5g9.jpg")`,
                     backgroundPosition: 'center',
                     backgroundSize: 'cover',
                     backgroundRepeat: 'no-repeat',
@@ -24,16 +28,22 @@ export default class HomePage extends React.Component {
                     height: '100vh',
                     backgroundOrigin: 'border-box',
                     backgroundAttachment: 'fixed!important',
-                    overflow: 'hidden'
+                    overflow: 'hidden',
                 }}>
             
                 <div >
-                    <h3>{"Genomics Browser"}</h3>
+                    <h1 className="title" >{"Genomics Browser"}</h1>
                 </div>
 
 
                 <div >
-                    <h4 >Helping Researchers and Medical Professionals Work With Genomics Data</h4> 
+                    <h4 className="title_caption">Helping Researchers and Medical Professionals Work With Genomics Data</h4> 
+                </div>
+
+
+                <div className="button_group">
+                    <button className="button_element" >Login</button>
+                    <button className="button_element" >Sign Up</button>
                 </div>
             </div>
             </div>
