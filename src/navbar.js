d0a34a604ab8daefd89911080d53ce125738ef1b
mehitaachuthan <z.quanticle@gmail.com>
2023-03-25 15:34:10 -0700

    changed front page navigation bar

--- src/navbar.js
+++ src/navbar.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import "./navbar.css";
 
+import { Link } from 'react-router-dom';
+
 function navbar() {
   return (
     <div>
-        <ul className='bar'>
+        {/*<ul className='bar'>
             <li className='bar'><a href='/' className='bar'>Home</a></li>
             <li className='bar'><a href='/' className='bar'>About</a></li>
             <li className='bar'><a href='/' className='bar'>Contact</a></li>
-        </ul>
+          </ul> */}
+        <div id='logo_header'>
+            <div id='logo_item'><img id='logo' src={process.env.PUBLIC_URL+ "/davis_logo.jpg"} /></div>
+        </div>
+        <div  className='outer_bar'>
+            <div className='bar_group'>
+                <div  className='bar_item'><Link className='bar_link'  to='/' >Home</Link></div>
+                <div  className='bar_item'><Link className='bar_link'  to='/' >About</Link></div>
+                <div  className='bar_item'><Link  className='bar_link' to='/' >Contact</Link></div>
+            </div>
+        </div>
     </div>
   )
 }
