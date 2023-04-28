d30e61c36c4c8a6ed975123b6947ac9d20ec513f
leej1230 <leejaewoo1230@gmail.com>
2023-04-27 21:35:59 -0700

    add static root on settings.py

--- genomics_browser_django_project/genomics_browser_django_project/settings.py
+++ genomics_browser_django_project/genomics_browser_django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b2qbs_a(h6)b-n#!r8o%#3&@1vnr%l3ef%y$%-&hqg&m-nyisl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '../build/static')
