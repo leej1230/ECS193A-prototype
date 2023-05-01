4eadfb1f3cd84b1a9a54d9badbf3dfce84455627
mehitaachuthan <z.quanticle@gmail.com>
2023-04-30 21:54:32 -0700

    test navigation from home page to links inside react components

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -11,3 +11,19 @@ describe('Test Upload Page', () => {
     cy.url().should('eq', 'https://ecs-193-a-prototype.vercel.app/upload')
   })
 })
+
+describe('Test Gene Page', () => {
+  it('Try to visit gene page', () => {
+    cy.visit('https://ecs-193-a-prototype.vercel.app/')
+    cy.contains('Gene Name:').click()
+    cy.url().should('includes', '/gene/')
+  })
+})
+
+describe('Test Dataset Page', () => {
+  it('Try to visit dataset page', () => {
+    cy.visit('https://ecs-193-a-prototype.vercel.app/')
+    cy.contains('Learn more').click()
+    cy.url().should('includes', '/dataset/')
+  })
+})
