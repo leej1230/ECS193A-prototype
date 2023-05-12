4e6a8a174b2432a4eeafca91e828a60e72a2b9c5
mehitaachuthan <z.quanticle@gmail.com>
2023-05-12 16:17:02 -0700

    added dropdown for user loging/account, some minor improvements are left

--- src/navbar.js
+++ src/navbar.js
@@ -11,9 +11,12 @@ import "./components/bootstrap_gene_page/css/sb-admin-2.min.css";
 
 function Navbar() {
 
-  const [dropdown, setDropdown] = useState(false);
+  const [dropdown_notifications, setDropdownNotifications] = useState(false);
+  const [dropdown_user, setDropdownUser] = useState(false);
+  const [num_notifications, set_num_notifications] = useState(3);
 
-  var menuClass = `dropdown-menu ${dropdown ? " show" : ""}`;
+  var menuClass = `dropdown-menu ${dropdown_notifications ? " show" : ""}`;
+  var userMenuClass = `dropdown-menu ${dropdown_user ? " show" : ""}`;
   
   return (
     <div>
@@ -42,15 +45,15 @@ function Navbar() {
           <li class="nav-item dropdown no-arrow mx-1" id="navbar_option_notification" >
                 <a class="nav-link dropdown-toggle" id="alertsDropdown" role="button"
                       data-toggle="dropdown" aria-haspopup="true" onClick={ async () => {
-                        if(dropdown == true){
-                            await setDropdown( false )
+                        if(dropdown_notifications == true){
+                            await setDropdownNotifications( false )
                         } else {
-                            await setDropdown( true )
+                            await setDropdownNotifications( true )
                         }
                 }}>
                         <i class="fas fa-bell fa-fw"></i>
 
-                        <span class="badge badge-danger badge-counter">3+</span>
+                        <span class="badge badge-danger badge-counter">{num_notifications}</span>
                 </a>
                 <div className={menuClass} aria-labelledby="alertsDropdown" id="notification_dropdown_menu_outer" >
                     
@@ -68,7 +71,7 @@ function Navbar() {
                             </div>
                         </a>
 
-                        {/*<a id="notifications_dropdown_item_body_second" class="dropdown-item d-flex align-items-center" href="#">
+                        <a id="notifications_dropdown_item_body_second" class="dropdown-item d-flex align-items-center" href="#">
                             <div class="mr-3">
                                 <div class="icon-circle bg-success">
                                     <i class="fas fa-donate text-white"></i>
@@ -90,98 +93,38 @@ function Navbar() {
                                 Spending Alert: We've noticed unusually high spending for your account.
                             </div>
                         </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>*/}
+                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
 
                 </div>
 
             </li>
 
-              {/*<li class="nav-item dropdown no-arrow mx-1">
-                  <li class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
-                      data-toggle="dropdown" aria-haspopup="true" aria-expanded={dropdown}  onClick={ async () => {
-                        if(dropdown == true){
-                            await setDropdown( false )
-                        } else {
-                            await setDropdown( true )
-                        }
-                        
-                      }}>
-                      <i class="fas fa-bell fa-fw"></i>
-
-                      <span class="badge badge-danger badge-counter">3+</span>
-                  </li>
-
-                  <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                      aria-labelledby="alertsDropdown">
-                      <h6 class="dropdown-header">
-                          Alerts Center
-                      </h6>
-                      <a class="dropdown-item d-flex align-items-center" href="#">
-                          <div class="mr-3">
-                              <div class="icon-circle bg-primary">
-                                  <i class="fas fa-file-alt text-white"></i>
-                              </div>
-                          </div>
-                          <div>
-                              <div class="small text-gray-500">December 12, 2019</div>
-                              <span class="font-weight-bold">A new monthly report is ready to download!</span>
-                          </div>
-                      </a>
-                      <a class="dropdown-item d-flex align-items-center" href="#">
-                          <div class="mr-3">
-                              <div class="icon-circle bg-success">
-                                  <i class="fas fa-donate text-white"></i>
-                              </div>
-                          </div>
-                          <div>
-                              <div class="small text-gray-500">December 7, 2019</div>
-                              $290.29 has been deposited into your account!
-                          </div>
-                      </a>
-                      <a class="dropdown-item d-flex align-items-center" href="#">
-                          <div class="mr-3">
-                              <div class="icon-circle bg-warning">
-                                  <i class="fas fa-exclamation-triangle text-white"></i>
-                              </div>
-                          </div>
-                          <div>
-                              <div class="small text-gray-500">December 2, 2019</div>
-                              Spending Alert: We've noticed unusually high spending for your account.
-                          </div>
-                      </a>
-                      <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
-                  </div>
-                </li>*/}
-
               <div class="topbar-divider d-none d-sm-block"></div>
 
-              <li class="nav-item dropdown no-arrow">
-                  <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <span class="mr-2 d-none d-lg-inline text-white-600 small">Douglas McGee</span>
-                  </a>
-
-                  <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                      aria-labelledby="userDropdown">
-                      <a class="dropdown-item" href="#">
+              <li class="nav-item dropdown no-arrow mx-1" id="user_account_drop_down" >
+                <a class="nav-link dropdown-toggle" id="userDropdown" role="button"
+                      data-toggle="dropdown" aria-haspopup="true" onClick={ async () => {
+                        if(dropdown_user == true){
+                            await setDropdownUser( false )
+                        } else {
+                            await setDropdownUser( true )
+                        }
+                }}>
+                        <div id="drop_down_login_user"><span class="mr-2 d-none d-lg-inline text-black-600 small" >Douglas McGee</span></div>
+                </a>
+                <div class={userMenuClass} aria-labelledby="userDropdown" id="user_dropdown_menu_outer">
+                    <a class="dropdown-item d-flex align-items-center" href="#" id="user_dropdown_item_body_profile">
                           <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                           Profile
-                      </a>
-                      <a class="dropdown-item" href="#">
-                          <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                          Settings
-                      </a>
-                      <a class="dropdown-item" href="#">
-                          <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                          Activity Log
-                      </a>
+                    </a>
                       <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                      <a class="dropdown-item d-flex align-items-center" href="#" data-toggle="modal" data-target="#logoutModal" id="user_dropdown_item_body_logout">
                           <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                           Logout
                       </a>
-                  </div>
-              </li> 
+                </div>
+
+            </li>
 
           </ul>
 
@@ -234,3 +177,32 @@ export default Navbar
               
           </div>
       </div> */
+
+
+      /*
+
+      <li class="nav-item dropdown no-arrow" id="user_account_drop_down">
+                  
+
+                  <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                      aria-labelledby="userDropdown">
+                      <a class="dropdown-item" href="#">
+                          <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                          Profile
+                      </a>
+                      <a class="dropdown-item" href="#">
+                          <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                          Settings
+                      </a>
+                      <a class="dropdown-item" href="#">
+                          <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                          Activity Log
+                      </a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                          Logout
+                      </a>
+                    </div>
+        </li> 
+      */
