8ef1e1502d071968e94499b98cb898144e016b7f
mehitaachuthan <z.quanticle@gmail.com>
2023-05-16 18:15:44 -0700

    fixed gene page number filter issue and bookmark issue

--- src/components/DatasetPage.js
+++ src/components/DatasetPage.js
@@ -980,7 +980,7 @@ function DatasetPage() {
                         }
                       }}
                     >
-                      {bookmarked ? <FontAwesomeIcon icon={icon({name: 'bookmark', style: 'regular' })} /> : <FontAwesomeIcon icon={icon({name: 'bookmark', style: 'regular' })} /> }
+                      {bookmarked ? <FontAwesomeIcon icon={icon({name: 'bookmark', style: 'solid' })} /> : <FontAwesomeIcon icon={icon({name: 'bookmark', style: 'regular' })} /> }
                     
                     </button>
                     
