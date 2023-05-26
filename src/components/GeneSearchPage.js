c98c7c32a2b1bf6c9560f3f4c6977a0f5b10299d
leej1230 <leejaewoo1230@gmail.com>
2023-05-25 23:54:32 -0700

    I forgor:skull: about how gene search api now returns differenct JSON

--- src/components/GeneSearchPage.js
+++ src/components/GeneSearchPage.js
@@ -14,7 +14,7 @@ import "./bootstrap_gene_page/css/sb-admin-2.min.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-import {clone} from "ramda";
+import { clone } from "ramda";
 
 import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
@@ -45,14 +45,14 @@ function GeneSearchPage() {
   const handleSearch = async () => {
     try {
       let search_input_for_url = clone(searchInput)
-      if( search_input_for_url == "" ){
+      if (search_input_for_url == "") {
         search_input_for_url = " ";
       }
       const response = await axios.get(
         `${process.env.REACT_APP_BACKEND_URL
         }/api/gene_search/${search_input_for_url}/${listPage.toString()}`
       );
-      setSearchResult(response.data);
+      setSearchResult(response.data.genes);
 
       console.log("search gene results: ")
       console.log(response.data)
@@ -107,46 +107,46 @@ function GeneSearchPage() {
   return (
     <body id="page-top">
       <div id="wrapper">
-        
+
 
         <div id="content-wrapper" class="d-flex flex-column">
           <div id="content">
-            
+
             <div class="container-fluid" id="full_gene_search_page">
-                <SideNav id="side_navigation_menu"
-                  onSelect={(selected) => {
-                      // Add your code here
-                  }}>
-                  <SideNav.Toggle />
-                    <SideNav.Nav defaultSelected="search_gene">
-                        <NavItem eventKey="home">
-                            <NavIcon>
-                                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em', color: 'white' }} />
-                            </NavIcon>
-                            <NavText style={{ color: 'white' }}>
-                                <a href="/console" style={{textDecoration: 'None'}}>Home</a>
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="search_gene">
-                            <NavIcon >
-                                <FontAwesomeIcon id="gene_icon" icon={icon({name: 'dna', style: 'solid' })} />
-                            </NavIcon>
-                            <NavText style={{ color: 'white' }}>
-                                <a href="/search_genes_page" style={{textDecoration: 'None'}}>Gene Search</a>
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="search_dataset" >
-                            <NavIcon >
-                                <FontAwesomeIcon id="dataset_search_icon" icon={icon({name: 'file', style: 'solid' })} />
-                            </NavIcon>
-                            <NavText style={{ color: 'white' }}>
-                              <a href="/search_datasets_page" style={{textDecoration: 'None'}}>Dataset Search</a>
-                            </NavText>
-                        </NavItem>
-                    </SideNav.Nav>
-                  </SideNav>
-                  
-              
+              <SideNav id="side_navigation_menu"
+                onSelect={(selected) => {
+                  // Add your code here
+                }}>
+                <SideNav.Toggle />
+                <SideNav.Nav defaultSelected="search_gene">
+                  <NavItem eventKey="home">
+                    <NavIcon>
+                      <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em', color: 'white' }} />
+                    </NavIcon>
+                    <NavText style={{ color: 'white' }}>
+                      <a href="/console" style={{ textDecoration: 'None' }}>Home</a>
+                    </NavText>
+                  </NavItem>
+                  <NavItem eventKey="search_gene">
+                    <NavIcon >
+                      <FontAwesomeIcon id="gene_icon" icon={icon({ name: 'dna', style: 'solid' })} />
+                    </NavIcon>
+                    <NavText style={{ color: 'white' }}>
+                      <a href="/search_genes_page" style={{ textDecoration: 'None' }}>Gene Search</a>
+                    </NavText>
+                  </NavItem>
+                  <NavItem eventKey="search_dataset" >
+                    <NavIcon >
+                      <FontAwesomeIcon id="dataset_search_icon" icon={icon({ name: 'file', style: 'solid' })} />
+                    </NavIcon>
+                    <NavText style={{ color: 'white' }}>
+                      <a href="/search_datasets_page" style={{ textDecoration: 'None' }}>Dataset Search</a>
+                    </NavText>
+                  </NavItem>
+                </SideNav.Nav>
+              </SideNav>
+
+
               <div id="gene_search_page_content">
 
                 <div id="title_box_gene_search">
@@ -164,64 +164,64 @@ function GeneSearchPage() {
                   />
 
                   <button type="submit" onClick={handleSearch} class="btn btn-primary" id="search_gene_button" aria-label="search">
-                    <i  class="fas fa-search"></i>
+                    <i class="fas fa-search"></i>
                   </button>
                 </div>
 
                 <div id="gene_search_results_display_container">
                   <div class="card shadow" id="gene_search_results_display">
-                    
-                        <SampleList resultList={searchResult} />
-                        {!hasSearched && (
-                          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minWidth: '100%', maxWidth: '100%' }}>
-                            Start Searching!
-                          </div>
-                        )}
-                        {hasSearched && searchResult.length === 0 && (
-                          <div>
-                            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minWidth: '100%', maxWidth: '100%' }}>
-                              No results
-                            </div>
-                            { listPage > 1 ? <div className="float-left">
-                              <button onClick={handleDecrementPage}>Prev Page</button>
-                            </div> : <div></div> }
-                          </div>
-                        )}
-                        {searchResult.length > 0 && (
-                        <div>
-                          { listPage > 1 ? <div className="float-left">
-                              <button onClick={handleDecrementPage}>Prev Page</button>
-                            </div> : <div></div> }
-                          <div className="float-right">
-                            <button onClick={handleIncrementPage}>Next Page</button>
-                          </div>
+
+                    <SampleList resultList={searchResult} />
+                    {!hasSearched && (
+                      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minWidth: '100%', maxWidth: '100%' }}>
+                        Start Searching!
+                      </div>
+                    )}
+                    {hasSearched && searchResult.length === 0 && (
+                      <div>
+                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minWidth: '100%', maxWidth: '100%' }}>
+                          No results
                         </div>
-                      )}
-                   
+                        {listPage > 1 ? <div className="float-left">
+                          <button onClick={handleDecrementPage}>Prev Page</button>
+                        </div> : <div></div>}
+                      </div>
+                    )}
+                    {searchResult.length > 0 && (
+                      <div>
+                        {listPage > 1 ? <div className="float-left">
+                          <button onClick={handleDecrementPage}>Prev Page</button>
+                        </div> : <div></div>}
+                        <div className="float-right">
+                          <button onClick={handleIncrementPage}>Next Page</button>
+                        </div>
+                      </div>
+                    )}
+
                   </div>
                 </div>
-       
+
                 <div id="bookmarked_genes_container" class="card">
-          
+
                   <div class="card-header py-3" id="bookmark_genes_card_header">
                     <h5 id="bookmark_genes_card_header_text">Bookmarked Genes</h5>
                   </div>
                   <div class="card-body" id="bookmark_genes_card_body">
-                        <SliderGene />
+                    <SliderGene />
                   </div>
-                  
+
                 </div>
-             
+
 
 
               </div>
 
 
             </div>
-          
+
+          </div>
         </div>
       </div>
-    </div>
 
       <script src="./bootstrap_gene_page/vendor/jquery/jquery.min.js"></script>
       <script src="./bootstrap_gene_page/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
