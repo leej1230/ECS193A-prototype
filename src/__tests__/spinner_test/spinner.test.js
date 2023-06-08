9edb3817352ef7141ed11ddd69136b30527ae6b5
qijunchen1922 <qijunchen19@gmail.com>
2023-06-08 01:48:59 -0700

    spinner test

---
+++ src/__tests__/spinner_test/spinner.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; 
+import LoadingSpinner from '../../components/spinner/spinner';
+
+jest.mock('../../components/spinner/gene.png', () => 'mocked-image-path');
+
+describe('LoadingSpinner', () => {
+  it('renders the image correctly', () => {
+    const { getByAltText } = render(<LoadingSpinner />);
+    const image = getByAltText('Dancing Rabbit');
+
+    expect(image).toBeInTheDocument();
+    expect(image.src).toContain('mocked-image-path');
+  });
+});
+
