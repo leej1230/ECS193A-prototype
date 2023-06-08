b0fab54569520f42d2bc4d12195c203e950cb445
qijunchen1922 <qijunchen19@gmail.com>
2023-06-08 14:16:06 -0700

    profile

--- src/__tests__/user_folder_test/profile.test.js
+++ src/__tests__/user_folder_test/profile.test.js
@@ -1,40 +1,38 @@
 import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'; 
+import axios from 'axios';
 import Profile from '../../components/user_folder/Contact';
+
+
+jest.mock('@auth0/auth0-react', () => ({
+    useAuth0: jest.fn(),
+  }));
 
 describe('Profile', () => {
     beforeEach(() => {
-        // Mock the useAuth0 return value
-        useAuth0.mockReturnValue({
-          user: {
-            sub: 'auth0|1234567890',
-            'https://unique.app.com/user_metadata': {
-              given_name: 'John',
-              family_name: 'Doe',
-            },
-          },
-        });
-    
-        // Mock axios get and post methods
-        jest.spyOn(axios, 'get').mockResolvedValue({ data: { bookmarked_genes: [], bookmarked_datasets: [], is_admin: false, is_staff: false } });
-        jest.spyOn(axios, 'post').mockResolvedValue({ data: [] });
-    
+    // Mock the useAuth0 hook implementation
+    useAuth0.mockReturnValue({
+      user: {
+        sub: 'auth0|1234567890',
+        'https://unique.app.com/user_metadata': {
+          given_name: 'John',
+          family_name: 'Doe',
+        },
+      },
+    });
+
+    // Mock axios get and post methods
+    jest.spyOn(axios, 'get').mockResolvedValue({ data: { bookmarked_genes: [], bookmarked_datasets: [], is_admin: false, is_staff: false } });
+    jest.spyOn(axios, 'post').mockResolvedValue({ data: [] });
+
         render(<Profile />);
-      });
+    });
 
-      afterEach(() => {
+    afterEach(() => {
         jest.clearAllMocks();
-      });
-
-      it('renders user information correctly', async () => {
-        // Wait for user information to be fetched
-        await waitFor(() => {
-          expect(screen.getByText('User full name: John Doe')).toBeInTheDocument();
-        });
-      });
-
+    });
 
 
 });
